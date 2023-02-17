34450e266e18d7f8ddddd2ad3d721221aa88a7fe
Marko Leinikka <mleinikka@gmail.com>
2023-02-17 20:31:38 +0900

    chore: Fix initial version number

--- internal/config/constants.go
+++ internal/config/constants.go
@@ -1,6 +1,6 @@
 package config
 
 const AppName = "Zeniire"
-const AppVersion = "0.0.1"
+const AppVersion = "0.1.0"
 const APIVersion = 1
 const GitLink = "https://github.com/CrescentKohana/Zeniire"
