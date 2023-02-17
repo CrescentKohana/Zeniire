5510dd8b9c1aaf1954568b394eb6c7918a7cb3c1
Marko Leinikka <mleinikka@gmail.com>
2023-02-17 17:01:16 +0900

    feat: Path to REST API

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"github.com/CrescentKohana/Zeniire/internal/config"
 	grpcAPI "github.com/CrescentKohana/Zeniire/pkg/api/grpc"
-	httpAPI "github.com/CrescentKohana/Zeniire/pkg/api/http"
+	restAPI "github.com/CrescentKohana/Zeniire/pkg/api/rest"
 )
 
 // main launches the client while loading environmentals, and initializing the gRPC and REST clients.
 func main() {
 	config.LoadEnv()
 	grpcAPI.InitGRPCClient()
-	httpAPI.InitRESTClient()
+	restAPI.InitRESTClient()
 }
